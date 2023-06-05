8d8636e73641882a4cbc0362b73621116366e226
Dele Paago <delebliss42@gmail.com>
2023-06-05 16:53:48 +0000

    Content changes

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -149,9 +149,9 @@ export const Contact = (props) => {
       <div id="footer">
         <div className="container text-center">
           <p>
-            &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
-            <a href="http://www.templatewire.com" rel="nofollow">
-              TemplateWire
+            &copy; 2023 Blissel Advisory. Design by{" "}
+            <a href="http://www.blisseladvisory.com" rel="nofollow">
+              Blissel Advisory
             </a>
           </p>
         </div>
