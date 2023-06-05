56df54c0e724892ae5c52c85c1911fada69235fc
Dele Paago <delebliss42@gmail.com>
2023-06-05 19:55:28 +0000

    Blurbs

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -7,8 +7,7 @@ export const Services = (props) => {
         <div className="section-title">
           <h2>Our Services</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+            Whatever your poultry needs, Dextraordinary Farm supplies! 
           </p>
         </div>
         <div className="row">
